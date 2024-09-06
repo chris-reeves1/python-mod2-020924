dab5d23e2ff7f4175ede9d0a02039d95e43a3457
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-09-06 10:54:11 +0000

    2

---
+++ files/boss.py
@@ -0,0 +1,8 @@
+import random
+import math
+
+def good_boos(name):
+    print(f"{name} is a good boss")
+
+def bad_boss(name):
+    print(f"{name} is a bad boss")
---
+++ files/files+modules.py
@@ -0,0 +1,66 @@
+# modules
+
+# libraries - such standard library. Can contain multiple packages and modules.
+# Packages - directories of python modules.  
+# modules - just python files. 
+
+# many built in and held in memory. Others need to imported. Other also we can install.
+# pip - is our package manager. 
+
+# pypi.org
+
+#import math
+
+#number = 10
+
+#answer = math.sqrt(number) # modulename.method(args)
+
+#print(answer)
+
+#import math
+#import random
+
+#def random_pi():
+#    return math.ceil(random.randint(1,10) * math.pi)
+
+#for i in range(5):
+#    print(random_pi())
+
+# just with desired objects to save memory
+
+#from math import ceil, floor, pi
+#from random import randint
+
+#def random_pi():
+ #   return floor(randint(1,20) * pi)
+
+#for i in range(5):
+    #print(random_pi())
+
+# exercise:
+# Create 2 files, one called dice.py - write a function that will
+# generate a random numer between 1 and 6.
+# in the second file import the dice module and simulate throwing 2 dice.
+# print its result.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
